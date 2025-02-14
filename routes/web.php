4254c8b4fcb2f2f251e3f57b4a1e661442d19941
Matheus Sousa <matheussousab2014@gmail.com>
2025-02-14 20:01:01 -0300

    feat: adiciona rotas e views para gerenciamento de pacientes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PacienteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+    
+    Route::resource('pacientes', PacienteController::class);
+});
