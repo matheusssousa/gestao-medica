485c24e18b4ee3547f42ec4be2c08ce10cbe1bb4
Matheus Sousa <matheussousab2014@gmail.com>
2025-02-15 09:37:13 -0300

    feat: adiciona estatísticas de atendimentos, médicos e pacientes na página inicial

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Atendimento;
+use App\Models\Medico;
+use App\Models\Paciente;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +26,27 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $counts = [
+            'atendimento_count' => Atendimento::count(),
+            'medicos_count' => Medico::count(),
+            'pacientes_count' => Paciente::count(),
+        ];
+
+        $ultimo_atendimento = Atendimento::latest()->first();
+
+        $medico_mais_atendimentos = Medico::withCount('atendimentos')->orderByDesc('atendimentos_count')->first();
+        $paciente_mais_atendimentos = Paciente::withCount('atendimentos')->orderByDesc('atendimentos_count')->first();
+
+        $atendimentosRecentes = Atendimento::latest()->take(5)->get();
+
+        return view('home', array_merge(
+            $counts,
+            compact(
+                'ultimo_atendimento',
+                'medico_mais_atendimentos',
+                'paciente_mais_atendimentos',
+                'atendimentosRecentes'
+            )
+        ));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 d-flex flex-column gap-3">
+                <div class="card" style="background-color: white;">
+                    <div class="card-header">
+                        <h5>Dashboard</h5>
+                    </div>
+                    <div class="card-body p-3">
+                        <h5>Estatísticas</h5>
+                        <hr>
+                        <div class="row mt-4">
+                            <div class="col">
+                                <ul>
+                                    <li>Atendimentos: {{ $atendimento_count }}</li>
+                                    <li>Médicos: {{ $medicos_count }}</li>
+                                    <li>Pacientes: {{ $pacientes_count }}</li>
+                                </ul>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <h5>Médico com Mais Atendimentos</h5>
+                        <hr>
+                        <div class="col">
+                            @if ($medico_mais_atendimentos)
+                                <p>{{ $medico_mais_atendimentos->nome }}
+                                    ({{ $medico_mais_atendimentos->atendimentos_count }} atendimentos)</p>
+                            @else
+                                <p>Nenhum médico encontrado</p>
+                            @endif
+                        </div>
+                        <h5>Paciente com Mais Atendimentos</h5>
+                        <hr>
+                        <div class="col">
+                            @if ($paciente_mais_atendimentos)
+                                <p>{{ $paciente_mais_atendimentos->nome }}
+                                    ({{ $paciente_mais_atendimentos->atendimentos_count }} atendimentos)</p>
+                            @else
+                                <p>Nenhum paciente encontrado</p>
+                            @endif
+                        </div>
+                        <h5>Último Atendimento</h5>
+                        <hr>
+                        <table class="table tabl-hover">
+                            <thead>
+                                <tr>
+                                    <th>Data de atendimento</th>
+                                    <th>Médico</th>
+                                    <th>Paciente</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if ($ultimo_atendimento)
+                                    <tr>
+                                        <td>{{ $ultimo_atendimento->data_atendimento->format('Y-m-d') }}</td>
+                                        <td>{{ $ultimo_atendimento->medico->nome }}</td>
+                                        <td>{{ $ultimo_atendimento->paciente->nome }}</td>
+                                    </tr>
+                                @else
+                                    <tr>
+                                        <td colspan="3">Nenhum atendimento encontrado</td>
+                                    </tr>
+                                @endif
+                        </table>
+                        <h5>Atendimentos Recentes</h5>
+                        <hr>
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Data de atendimento</th>
+                                    <th>Médico</th>
+                                    <th>Paciente</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($atendimentosRecentes as $atendimento)
+                                    <tr>
+                                        <td>{{ $atendimento->data_atendimento->format('Y-m-d') }}</td>
+                                        <td>{{ $atendimento->medico->nome }}</td>
+                                        <td>{{ $atendimento->paciente->nome }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
